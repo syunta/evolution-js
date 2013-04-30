85687f784f6229d8c9872da225742bad924f11e6
syunta kitagawa <kitagawa@timedia.co.jp>
2013-04-30 13:48:17 +0900

    html：追加：関数drawWorldを追加

--- evolution.js
+++ evolution.js
@@ -1,3 +1,18 @@
 function skipDay(){}
 
 function updateWorld(){}
+
+function drawWorld(){
+	
+	var world = "";
+
+	for(var i = 0; i < 30; i++){
+		for(var j = 0; j < 100; j++){
+			world += ".";
+		}
+		world += "<br>";
+	}
+	
+	document.getElementById("world").innerHTML = world;
+
+}
